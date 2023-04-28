cbaf9fad29f7c73a053d5d18384b11ecba7c8cfe
sazgez <wishell.rock.4@gmail.com>
2023-04-28 14:46:03 +0300

    step-4

--- app/src/main/java/com/pismamo/rockpaperscissorapp/MainActivity.java
+++ app/src/main/java/com/pismamo/rockpaperscissorapp/MainActivity.java
@@ -1,18 +1,24 @@
 package com.pismamo.rockpaperscissorapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView enemy_score, my_score, enemy_choice, my_choice;
-    Button button_rock, button_paper, button_scissor, button_restart, button_exit;
-    int enemyScore = 0, myScore = 0;
+    TextView enemy_score, my_score;
+    ImageView enemy_choice, my_choice;
+    ImageButton button_rock, button_paper, button_scissor;
+    Button button_restart, button_exit;
+    int enemyScore = 0, myScore = 0, win = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         enemy_score = findViewById(R.id.textViewEnemyScore);
         my_score = findViewById(R.id.textViewMyScore);
-        enemy_choice = findViewById(R.id.textViewEnemyChoice);
-        my_choice = findViewById(R.id.textViewMyChoice);
+        enemy_choice = findViewById(R.id.imageViewEnemyChoice);
+        my_choice = findViewById(R.id.imageViewMyChoice);
         button_rock = findViewById(R.id.buttonRock);
         button_paper = findViewById(R.id.buttonPaper);
         button_scissor = findViewById(R.id.buttonScissor);
@@ -32,12 +38,12 @@ public class MainActivity extends AppCompatActivity {
         button_restart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enemy_score.setText("0");
-                my_score.setText("0");
-                enemy_choice.setText("");
-                my_choice.setText("");
                 enemyScore = 0;
                 myScore = 0;
+                enemy_score.setText("0");
+                my_score.setText("0");
+                enemy_choice.setImageResource(R.drawable.bluepill);
+                my_choice.setImageResource(R.drawable.redpill);
             }
         });
         button_exit.setOnClickListener(new View.OnClickListener() {
@@ -52,15 +58,15 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId())
         {
             case R.id.buttonRock:
-                my_choice.setText("ROCK");
+                my_choice.setImageResource(R.drawable.rock);
                 start(1);
                 break;
             case R.id.buttonPaper:
-                my_choice.setText("PAPER");
+                my_choice.setImageResource(R.drawable.paper);
                 start(2);
                 break;
             case R.id.buttonScissor:
-                my_choice.setText("SCISSOR");
+                my_choice.setImageResource(R.drawable.scissor);
                 start(3);
                 break;
         }
@@ -70,11 +76,11 @@ public class MainActivity extends AppCompatActivity {
         int enemyChoice = (int)(Math.random()*3+1);
 
         if(enemyChoice == 1)
-            enemy_choice.setText("ROCK");
+            enemy_choice.setImageResource(R.drawable.rock);
         if(enemyChoice == 2)
-            enemy_choice.setText("PAPER");
+            enemy_choice.setImageResource(R.drawable.paper);
         if(enemyChoice == 3)
-            enemy_choice.setText("SCISSOR");
+            enemy_choice.setImageResource(R.drawable.scissor);
 
         if((enemyChoice == 1 && myChoice == 1) ||
                 (enemyChoice == 2 && myChoice == 2) ||
@@ -93,6 +99,64 @@ public class MainActivity extends AppCompatActivity {
         {
             myScore++;
             my_score.setText(String.valueOf(myScore));
+        }
+        if(enemyScore == win || myScore == win)
+        {
+            alertMessage();
+        }
+    }
+
+    public void alertMessage() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        if(enemyScore == win)
+        {
+            dialog.setIcon(R.drawable.herobrine);
+            dialog.setTitle("UNFORTUNATELY, YOU LOST!");
+            dialog.setMessage("Enemy Score: "+enemyScore+"\nYour Score: "+myScore+"\nWanna take revenge?");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("LET'S DO IT!", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    enemyScore = 0;
+                    myScore = 0;
+                    enemy_score.setText("0");
+                    my_score.setText("0");
+                    enemy_choice.setImageResource(R.drawable.bluepill);
+                    my_choice.setImageResource(R.drawable.redpill);
+                }
+            });
+            dialog.setNegativeButton("NOPE", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    System.exit(0);
+                }
+            });
+            dialog.show();
+        }
+        else
+        {
+            dialog.setIcon(R.drawable.trophy);
+            dialog.setTitle("CONGRATS, YOU WON!");
+            dialog.setMessage("Enemy Score: "+enemyScore+"\nYour Score: "+myScore+"\nWanna win one more time?");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("SURE!", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    enemyScore = 0;
+                    myScore = 0;
+                    enemy_score.setText("0");
+                    my_score.setText("0");
+                    enemy_choice.setImageResource(R.drawable.bluepill);
+                    my_choice.setImageResource(R.drawable.redpill);
+                }
+            });
+            dialog.setNegativeButton("NEXT TIME", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    System.exit(0);
+                }
+            });
+            dialog.show();
         }
     }
 }
